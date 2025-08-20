3e09abfa08a0ec1193fa23d9ffbca7e1a7040cdf
mrs-reboot <devsecmail2@gmail.com>
2025-08-20 19:23:00 +0200

    Python exercises

---
+++ 6.100L Finger Exercises/lecture_5.py
@@ -0,0 +1,19 @@
+# Assume you are given a string variable named my_str. Write a piece of Python code that prints out a new string containing the even indexed characters of my_str. For example, if my_str = "abcdefg" then your code should print out aceg.
+
+
+
+my_str = "abcdefg"
+
+
+
+# 1 sneaky
+print(my_str[::2])
+
+# 2 loop practice
+result = ""
+for i in range(len(my_str)):
+    if i % 2 == 0:
+        result += my_str[i]
+print(result)
+    
+    
