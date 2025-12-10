3a568cfbfbecc4fef86dfbba0ca7a43a6b204a89
exitsun <devsecmail2@gmail.com>
2025-12-10 15:01:02 +0100

    cd

---
+++ javascript/11-todo-list.js
@@ -0,0 +1,35 @@
+allTasks = [];
+      renderToDoList();
+
+      function renderToDoList() {
+        let todoListHTML = ``;
+
+        for (let i = 0; i < allTasks.length; i++) {
+          const todoObject = allTasks[i];
+          const name = todoObject.name;
+          const dueDate = todoObject.dueDate;
+          const html = `
+          <div> ${name}</div>
+          <div>${dueDate}</div>
+          <div>
+            <button
+            class="delete-todo-button";onclick="allTasks.splice(${i}, 1);
+            renderToDoList();
+             "
+            >Delete</button>
+          </div>
+           `;
+          todoListHTML += html;
+        }
+        document.querySelector(".tasks-container").innerHTML = todoListHTML;
+      }
+
+      function addToDo() {
+        const secondInputField = document.getElementById("todo-second");
+        const dateInputElement = document.getElementById("calendar");
+        const dueDate = dateInputElement.value;
+        const name = secondInputField.value;
+        allTasks.push({ name: name, dueDate: dueDate }); //name, dueDate <- shorthand property syntax
+        secondInputField.value = "";
+        renderToDoList();
+      }
