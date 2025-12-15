01e004e952147429fce83be98b61df670d0ddb8a
exitsun <devsecmail2@gmail.com>
2025-12-15 17:38:08 +0100

    remove from cart

--- javascript/amazon-project/data/cart.js
+++ javascript/amazon-project/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -26,4 +26,17 @@ export function addToCart(productId, selectedValue) {
       quantity: Number(selectedValue),
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((item) => {
+    if (item.productId !== productId) {
+      newCart.push(item);
+    }
+  });
+
+  cart = newCart;
+  console.log(cart);
 }
