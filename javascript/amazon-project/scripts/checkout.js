85bdd63b40be5d038ddb6f96477da45e08602fb9
exitsun <devsecmail2@gmail.com>
2025-12-15 17:25:02 +0100

    fixed radio selectors

--- javascript/amazon-project/scripts/checkout.js
+++ javascript/amazon-project/scripts/checkout.js
@@ -45,21 +45,21 @@ cart.forEach((cartItem) => {
                   Choose a delivery option:
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" checked="" class="delivery-option-input" name="delivery-option-1">
+                  <input type="radio" checked="" class="delivery-option-input" name="delivery-option-${productId}">
                   <div>
                     <div class="delivery-option-date">Tuesday, June 21</div>
                     <div class="delivery-option-price">FREE Shipping</div>
                   </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-1">
+                  <input type="radio" class="delivery-option-input" name="delivery-option-${productId}">
                   <div>
                     <div class="delivery-option-date">Wednesday, June 15</div>
                     <div class="delivery-option-price">$4.99 - Shipping</div>
                   </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-1">
+                  <input type="radio" class="delivery-option-input" name="delivery-option-${productId}">
                   <div>
                     <div class="delivery-option-date">Monday, June 13</div>
                     <div class="delivery-option-price">$9.99 - Shipping</div>
