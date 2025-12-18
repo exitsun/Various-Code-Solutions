96abf0036d35363788dbfd3c560c445d72dff8dc
exitsun <devsecmail2@gmail.com>
2025-12-18 16:14:08 +0100

    products into classes

--- javascript/amazon-project/scripts/amazon.js
+++ javascript/amazon-project/scripts/amazon.js
@@ -3,7 +3,6 @@ import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { updateCartQuantity } from "../data/cart.js";
 
-console.log("hello");
 let productsHTML = "";
 updateCartQuantity();
 
@@ -21,14 +20,14 @@ products.forEach((product) => {
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars * 10}.png">
+          src="${product.getStarsUrl()}">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
       </div>
 
       <div class="product-price">
-        $${formatCurrency(product.priceCents)}
+        $${product.getPrice()}
       </div>
 
       <div class="product-quantity-container ">
