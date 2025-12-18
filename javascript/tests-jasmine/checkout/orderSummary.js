e9407bfe7ed551f6e3e1f266a60730c8153b33d5
exitsun <devsecmail2@gmail.com>
2025-12-18 14:23:01 +0100

    oop cart model

---
+++ javascript/tests-jasmine/checkout/orderSummary.js
@@ -0,0 +1,7 @@
+import { renderOrderSummary } from "Various-Code-Solutions/javascript/amazon-project/scripts/checkout/orderSummary.js";
+
+describe("test suite: renderOrderSummary", () => {
+  it("displays the cart", () => {
+    renderOrderSummary;
+  });
+});
