d5987eb79d5fa41c6cd4c1b0aceefa028266e002
exitsun <devsecmail2@gmail.com>
2025-12-22 16:21:59 +0100

    fetch added

--- javascript/amazon-project/data/products.js
+++ javascript/amazon-project/data/products.js
@@ -71,26 +71,51 @@ class Appliance extends Product {
 
 export let products = [];
 
-export function loadProducts(fun) {
-  const xhr = new XMLHttpRequest();
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productsData) => {
+      // console.log(productsData);
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        } else if (productDetails.type === "appliance") {
+          return new Appliance(productDetails);
+        }
 
-  xhr.addEventListener("load", () => {
-    products = JSON.parse(xhr.response).map((productDetails) => {
-      if (productDetails.type === "clothing") {
-        return new Clothing(productDetails);
-      } else if (productDetails.type === "appliance") {
-        return new Appliance(productDetails);
-      }
-
-      return new Product(productDetails);
-    });
-    console.log("load products");
-
-    fun();
-  });
-  xhr.open("GET", "https://supersimplebackend.dev/products");
-  xhr.send();
+        return new Product(productDetails);
+      });
+      console.log("load products");
+    }); //fetch uses a promise to make an xhr request and also parses the JSON
+  return promise;
 }
+
+// loadProductsFetch().then(() => {
+//   console.log("nextstep");
+// });
+
+// export function loadProducts(fun) {
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.addEventListener("load", () => {
+//     products = JSON.parse(xhr.response).map((productDetails) => {
+//       if (productDetails.type === "clothing") {
+//         return new Clothing(productDetails);
+//       } else if (productDetails.type === "appliance") {
+//         return new Appliance(productDetails);
+//       }
+
+//       return new Product(productDetails);
+//     });
+//     console.log("load products");
+
+//     fun();
+//   });
+//   xhr.open("GET", "https://supersimplebackend.dev/products");
+//   xhr.send();
+// }
 
 // export const products = [
 //   {
